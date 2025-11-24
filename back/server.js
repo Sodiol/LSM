ab1869476c4ddc5802a1e6bef91db45fb13fcd0e
Sodiol <91626926+Sodiol@users.noreply.github.com>
2025-11-24 13:38:20 -0600

    for azure
    Modificado para ser usado en Azure

--- back/server.js
+++ back/server.js
@@ -73,6 +73,6 @@ app.use('/api', detectRoutes);
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  open(`http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
